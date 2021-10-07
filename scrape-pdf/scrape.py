8739425befc0905cbca6ca893e86900a13391d21
Joseph Cammarata <cammarj@email.sc.edu>
2021-10-07 13:20:21 -0400

    Threw together a script to just get the markers on campus. that json file is included as well as the script and the json from scrape.py

--- scrape-pdf/scrape.py
+++ scrape-pdf/scrape.py
@@ -1,48 +1,50 @@
 import json
 import re
 
-#reg: (\\n( )*){2,}) matches 3 consecutive
-#reg: (\\n(?!(( )*\\n)+)) should match all single instances leaving one for 2 and 2 for 3
-#reg: (\d){1,2}-(\d){1,2}  matches ##-## format without matching years
+
+# reg: (\\n( )*){2,}) matches 3 consecutive
+# reg: (\\n(?!(( )*\\n)+)) should match all single instances leaving one for 2 and 2 for 3
+# reg: (\d){1,2}-(\d){1,2}  matches ##-## format without matching years
 
 def editLine(line):
-    #Removes newline characters at end
+    # Removes newline characters at end
     line = line.strip()
-    #Removes footnote references
-    line = line.replace('*','')
-    line = line.replace('†','')
-    line = line.replace('‡','')
-    line = line.replace('§','')
-    #Replaces ? in GPS coordinates
-    line = line.replace('?','\'')
-    #Fixes unicode
+    # Removes footnote references
+    line = line.replace('*', '')
+    line = line.replace('†', '')
+    line = line.replace('‡', '')
+    line = line.replace('§', '')
+    # Replaces ? in GPS coordinates
+    line = line.replace('?', '\'')
+    # Fixes unicode
     line = line.replace('\u2019', '\'')
     return line
 
+
 def coordinates(line):
-    #GPS Coordinates: 34° 8.102' N, 82° 24.856' W
+    # GPS Coordinates: 34° 8.102' N, 82° 24.856' W
     line = line[17:]
-    line = line.replace('°',' ')
-    line = line.replace('\'',' ')
-    line = line.replace(',','')
-    line = line.replace('’','')
-    line = line.replace('N','')
-    line = line.replace('W','')
+    line = line.replace('°', ' ')
+    line = line.replace('\'', ' ')
+    line = line.replace(',', '')
+    line = line.replace('’', '')
+    line = line.replace('N', '')
+    line = line.replace('W', '')
     line = line.split()
-    #34 8.102 N 82 24.856 W
-    #Formulas: D.d = Degrees + (Minutes.m / 60)
-    #Always N and W
-    lat = int(line[0]) + (float(line[1]) / 60) 
+    # 34 8.102 N 82 24.856 W
+    # Formulas: D.d = Degrees + (Minutes.m / 60)
+    # Always N and W
+    lat = int(line[0]) + (float(line[1]) / 60)
     lon = int(line[2]) + (float(line[3]) / 60)
-    return (lat,lon)
-    
-    
-#file = "South Carolina Historical Markers (8-19-21).pdf"
+    return (lat, lon)
+
+
+# file = "South Carolina Historical Markers (8-19-21).pdf"
 file = 'markers_information.txt'
-f = open(file,"r")
+f = open(file, "r")
 lines = []
 lines = f.readlines()
-#output = []
+# output = []
 json_out = []
 
 county = ""
@@ -55,19 +57,19 @@ digitsPattern = re.compile('^((\d){1,2}-(\d){1,3} )')
 while (i < length - 1):
     i += 1
     line = lines[i]
-    #Ignores footnotes (for now)
+    # Ignores footnotes (for now)
     if (line[0] == '*' or line[0] == '†' or line[0] == '‡' or line[0] == '§'):
         continue
     line = editLine(line)
-    
-    #Sets current county when found
+
+    # Sets current county when found
     startsWDigit = startPattern.match(line)
     if (line[-6:] == "COUNTY" and startsWDigit == None):
         county = line
 
-    #Matches start of entry digit-digit
+    # Matches start of entry digit-digit
     if (digitsPattern.match(line)):
-        #Removes digits from name
+        # Removes digits from name
         name = ' '.join((line.split())[1:])
         rel_loc = editLine(lines[i + 1])
         desc = editLine(lines[i + 2])
@@ -88,13 +90,13 @@ while (i < length - 1):
             "county": county
         }
         json_out.append(d)
-        #output.append(d) #Test list of dictionaries (mid-step to json output)
+        # output.append(d) #Test list of dictionaries (mid-step to json output)
         i += skip
 
-#Print dictionary list
-#for entry in output:
+# Print dictionary list
+# for entry in output:
 #    print(entry)
 
-#JSON output code
-with open('json_markers.txt', 'w') as outfile:
+# JSON output code
+with open('json_markers.json', 'w') as outfile:
     json.dump(json_out, outfile, indent=4)
