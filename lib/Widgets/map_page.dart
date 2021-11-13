6b35073f4a4146cfde4a27ef43092a8360aae60b
mtduggan <mtduggan@email.sc.edu>
2021-11-13 01:11:38 -0500

    added mapbox

---
+++ lib/Widgets/map_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
+
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: new MapOptions(
+        minZoom: 13.0,
+        center: latLng.LatLng(34.0007, -81.0348),
+        zoom: 13.0,
+      ),
+      layers: [
+        TileLayerOptions(
+            urlTemplate: "https://api.mapbox.com/styles/v1/mtduggan/ckukb9uuk6wcz18p9dxyqd1ps/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibXRkdWdnYW4iLCJhIjoiY2t1a2I4MTV5MWE2MzJ3b2YycGl0djRnZyJ9.Sx7oMnrNlA1yWBO42iSAOQ",
+            additionalOptions: {
+              'accessToken': 'pk.eyJ1IjoibXRkdWdnYW4iLCJhIjoiY2t1a2I4MTV5MWE2MzJ3b2YycGl0djRnZyJ9.Sx7oMnrNlA1yWBO42iSAOQ',
+              'id': 'mapbox.satellite',
+
+            }
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 45.0,
+              height: 45.0,
+              point: latLng.LatLng(34.0007, -81.0348),
+              builder: (ctx) =>
+              new Container(
+                child: IconButton(
+                  icon: Icon(Icons.location_on),
+                  color: Colors.red,
+                  iconSize: 45,
+                  onPressed: (){
+                    print('My Marker');
+                  },
+                ),
+              ),
+            ),
+          ],
+        )
+      ]
+    );
+  }
+}
