2d5c5b76fa4a6973ee93c175fef31d16ab92d757
Lauren <lehodges@email.sc.edu>
2021-11-26 16:47:55 -0500

    Created account creation page

---
+++ lib/Widgets/account_creation_page.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+// import '../main.dart';
+import 'account_page.dart';
+
+// TODO add firebase, including email + password and Google login capabilities
+FirebaseAuth auth = FirebaseAuth.instance;
+
+class annount_creation extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+//TODO create account creation form, implement firebase logging and storing
