1bd6d6a55e13a196c681e7d11fc3e427b03e36be
Lauren <lehodges@email.sc.edu>
2021-11-28 17:46:16 -0500

    basis for account creation form

--- lib/Widgets/account_creation_page.dart
+++ lib/Widgets/account_creation_page.dart
@@ -7,11 +7,124 @@ import 'account_page.dart';
 // TODO add firebase, including email + password and Google login capabilities
 FirebaseAuth auth = FirebaseAuth.instance;
 
-class annount_creation extends StatefulWidget {
+class AccountForm extends StatefulWidget {
+  const AccountForm({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  AccountState createState() => AccountState();
+}
+
+class AccountState extends State<AccountForm> {
+  final _formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'First Name',
+                    hintText: 'Enter your First Name',
+                  ),
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your first name.';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Last Name',
+                    hintText: 'Enter your Last Name',
+                  ),
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your last name.';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter your email address',
+                  ),
+
+                  validator: (value) {
+                    bool sym;
+                    if (value != null && value.contains('@')) {
+                      sym = true;
+                    }
+                    else {
+                      sym = false;
+                    }
+                    if (value == null || value.isEmpty || !sym) {
+                      return 'Please enter a valid email address.';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter a password',
+                  ),
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty || value.length < 8) {
+                      return 'Password must have at least 8 characters';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey, borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context, MaterialPageRoute(builder: (_) => AccountPage()));
+                  },
+                  child: Text(
+                    'Create Account',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ]
+        )
+    )
+    );
   }
 }
 
