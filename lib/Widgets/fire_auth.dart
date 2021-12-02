3899ad6817821b450cf0cf69ba605090c375617c
Lauren <lehodges@email.sc.edu>
2021-12-01 22:23:04 -0500

    basic auth methods created

---
+++ lib/Widgets/fire_auth.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FireAuth {
+  static Future<User?> registerUsingEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCred = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      user = userCred.user;
+    }
+    on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('Account already exists for that email.');
+      }
+    }
+    catch (e) {
+      print(e);
+    }
+    return user;
+  }
+
+
+  static Future<User?> signInUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Password is incorrect');
+      }
+    }
+
+    return user;
+  }
+}
