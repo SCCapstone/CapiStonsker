43bb799a039d9aa16ad1b4de6e44dbb1942f6c36
Lauren <lehodges@email.sc.edu>
2021-12-02 17:25:20 -0500

    google sign in method added

--- lib/Widgets/fire_auth.dart
+++ lib/Widgets/fire_auth.dart
@@ -1,6 +1,10 @@
+import 'dart:js';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 FirebaseAuth auth = FirebaseAuth.instance;
+GoogleSignIn googleSignIn = GoogleSignIn();
 
 
 class FireAuth {
@@ -53,8 +57,21 @@ class FireAuth {
     return user;
   }
 
-
-
-
+  Future<String?> signInwithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+      await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      await auth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+      throw e;
+    }
+  }
 }
 
