3f2dafd8fd8b445f5545428792636431fe594b17
Joseph Cammarata <cammarj@email.sc.edu>
2022-02-25 23:23:42 -0500

    Camera Functionality Addded. Still needs aesthetics changes and to link to firebase

--- lib/auth/sign_up_page.dart
+++ lib/auth/sign_up_page.dart
@@ -18,125 +18,127 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUp extends State<SignUp> {
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       key: _scaffoldKey,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: new IconButton(
-            icon: const Icon(Icons.arrow_back),
-            color: Colors.white,
-            onPressed: () {
-              Navigator.of(context).pop();
-            }
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: new IconButton(
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pop();
+              }
+          ),
+          title: Text("Sign Up"),
+          backgroundColor: Colors.blueGrey,
         ),
-        title: Text("Sign Up"),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
-            Widget>[
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Text("Sign Up",
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text("Sign Up",
                 style: TextStyle(
                     fontSize: 50,
                     color: Colors.black,
                     fontWeight: FontWeight.bold
                 )
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Container(
-              height: 50,
-              width: 500,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => AccountCreation()),
-                  );
-                },
-                child: Text(
-                  'Sign up with Email',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Container(
-              height: 50,
-              width: 500,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () async {
-                  try {
-                    await FireAuth.signInWithGoogle(context: context) ;
-
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pop();
-
-                  } on FirebaseAuthException catch (e) {
-                    showDialog(
-                      context: context,
-                      builder: (ctx) =>
-                          AlertDialog(
-                            title: Text("Login Failed. Please try again."),
-                            content: Text('${e.message}'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(ctx).pop();
-                                },
-                                child: Text('Okay'),
-                              )
-                            ],
-                          ),
-                    );
-                    print(e);
-                  }
-                },
-                child: Text(
-                  'Log in with Google',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-
-            ),
-          ),
-
-          Padding(
+            Padding(
               padding: EdgeInsets.all(10.0),
-              child: GestureDetector(
-                  child: Text("Already a user? Log In",
-                      style: TextStyle(
-                          fontSize: 20,
-                          decoration: TextDecoration.underline,
-                          color: Colors.blue)),
-                  onTap: () {
-                    //popUntil account
+              child: Container(
+                height: 50,
+                width: 500,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  key: const Key('emailSignUp'),
+                  onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          settings: RouteSettings(name: "/login"),
-                          builder: (context) => LoginScreen()),
+                          builder: (context) => AccountCreation()),
                     );
-                  }))
-        ]),
-      ),
-      drawer: SideMenu(),
-      bottomNavigationBar: BottomNavBar(scaffoldKey: _scaffoldKey,),
+                  },
+                  child: Text(
+                    'Sign up with Email',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Container(
+                height: 50,
+                width: 500,
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () async {
+                try {
+                await FireAuth.signInWithGoogle(context: context) ;
+
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+                
+                } on FirebaseAuthException catch (e) {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) =>
+                        AlertDialog(
+                          title: Text("Login Failed. Please try again."),
+                          content: Text('${e.message}'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                              child: Text('Okay'),
+                            )
+                          ],
+                        ),
+                  );
+                  print(e);
+                }
+                },
+                  child: Text(
+                    'Log in with Google',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+
+              ),
+            ),
+
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    child: Text("Already a user? Log In",
+                        style: TextStyle(
+                            fontSize: 20,
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue)),
+                    onTap: () {
+                      //popUntil account
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            settings: RouteSettings(name: "/login"),
+                            builder: (context) => LoginScreen()),
+                      );
+                    }))
+          ]),
+        ),
+        drawer: SideMenu(),
+        bottomNavigationBar: BottomNavBar(scaffoldKey: _scaffoldKey,),
     );
   }}
