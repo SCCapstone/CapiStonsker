cc397a201d2c21b57d732ff2d69881330b254bf6
Lauren Hodges <lehodges@email.sc.edu>
2022-04-03 17:18:49 -0400

    ignore

--- lib/auth/fire_auth.dart
+++ lib/auth/fire_auth.dart
@@ -1,7 +1,7 @@
 /*
  * This page provides Firebase user authentication functionality,
  * which includes signing in or creating an account using
- * an email and password as well as Google sign in
+ * an email and password
  *
  * These functions are implemented on the log_in_page and
  * account_creation_page
@@ -11,10 +11,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:capi_stonsker/markers/locations.dart' as locs;
-import 'package:google_sign_in/google_sign_in.dart';
+
 
 //FirebaseAuth auth = FirebaseAuth.instance;
-GoogleSignIn googleSignIn = GoogleSignIn();
+
 
 
 class FireAuth {
@@ -81,42 +81,6 @@ class FireAuth {
   }
 
   //TODO Add list retrieves and friend subscription
-  // This method allows a user to sign in using their Google account
-  static Future<User?> signInWithGoogle({required BuildContext context}) async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
-
-    final GoogleSignIn googleSignIn = GoogleSignIn();
-
-    final GoogleSignInAccount? googleSignInAccount =
-    await googleSignIn.signIn();
-
-    if (googleSignInAccount != null) {
-      final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
-
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-      );
-
-      try {
-        final UserCredential userCredential =
-        await auth.signInWithCredential(credential);
-
-        user = userCredential.user;
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'account-exists-with-different-credential') {
-          print('Incorrect credentials.');
-        }
-        else if (e.code == 'invalid-credential') {
-          print('Invalid credentials');
-        }
-      }
-    }
-
-    return user;
-  }
 
   static Future<String> getEmail() async {
     return (await auth.currentUser)!.email!;
